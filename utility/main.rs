91c561cc1fb7ef0b50c4f1200fb9711cc14de950
Siddharth <siddharth.060605@gmail.com>
2026-01-19 02:27:27 -0500

    A teensy update

--- utility/main.rs
+++ utility/main.rs
@@ -1,9 +1,15 @@
 use std::{fs::File, io::Read};
+use std::env;
 
+use antlr_rust::InputStream;
+use antlr_rust::token_stream::TokenStream;
+
+use crate::clexer::CLexer;
 use crate::cparser::CParser;
 
-
 mod cparser;
+mod clexer;
+mod clistener;
 
 pub fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,5 +19,10 @@ pub fn main() {
     let mut file_content = String::new();
     File::open(path).unwrap().read_to_string(&mut file_content).unwrap();
     
-    CParser::new(file_content)
+    let lexer = CLexer::new(InputStream::new(file_content));
+    let parser = CParser::new(TokenStream::new(lexer));
+
+    parser.
+
+   
 }
