8b5c575e18e6da75f65c2b57c5fcd03f82cdd609
Siddharth Narayan <siddharth.060605@gmail.com>
2026-01-22 23:10:49 -0600

    Update flake.nix -- try to be more platform agnostic

--- flake.nix
+++ flake.nix
@@ -6,25 +6,83 @@
     fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = {
-    self,
-    flake-utils,
-    naersk,
-    nixpkgs,
-    fenix,
-  }:
+  outputs =
+    {
+      self,
+      flake-utils,
+      naersk,
+      nixpkgs,
+      fenix,
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = (import nixpkgs) {
           inherit system;
-          overlays = [fenix.overlays.default];
+          overlays = [ fenix.overlays.default ];
         };
 
-        naersk' = pkgs.callPackage naersk {};
-      in rec {
-        defaultPackage = naersk'.buildPackage {
+        toolchain =
+          with fenix.packages.${system};
+          combine [
+            minimal.rustc
+            minimal.cargo
+            targets.x86_64-unknown-linux-musl.latest.rust-std
+            targets.x86_64-pc-windows-gnu.latest.rust-std
+            targets.i686-pc-windows-gnu.latest.rust-std
+          ];
+
+        naersk' = naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+        naerskBuildPackage =
+          target: args: naersk'.buildPackage (cargoConfig // args // { CARGO_BUILD_TARGET = target; });
+
+        # All of the CARGO_* configurations which should be used for all
+        # targets.
+        #
+        # Only use this for options which should be universally applied or which
+        # can be applied to a specific target triple.
+        #
+        # This is also merged into the devShell.
+        cargoConfig = {
           src = ./.;
           copyLibs = true;
+
+          # Tells Cargo to enable static compilation.
+          # (https://doc.rust-lang.org/cargo/reference/config.html#targettriplerustflags)
+          #
+          # Note that the resulting binary might still be considered dynamically
+          # linked by ldd, but that's just because the binary might have
+          # position-independent-execution enabled.
+          # (see: https://github.com/rust-lang/rust/issues/79624#issuecomment-737415388)
+          CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_RUSTFLAGS = "-C target-feature=+crt-static";
+
+          # Tells Cargo that it should use Wine to run tests.
+          # (https://doc.rust-lang.org/cargo/reference/config.html#targettriplerunner)
+          CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUNNER = pkgs.writeScript "wine-wrapper" ''
+            export WINEPREFIX="$(mktemp -d)"
+            exec wine64 $@
+          '';
+        };
+      in
+      rec {
+        packages.default = packages.linux;
+
+        packages.linux = naerskBuildPackage "x86_64-unknown-linux-gnu" {};
+
+        packages.windows = naerskBuildPackage "x86_64-pc-windows-gnu" {
+          depsBuildBuild = with pkgs; [
+            pkgsCross.mingwW64.stdenv.cc
+            pkgsCross.mingwW64.windows.pthreads
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            # We need Wine to run tests:
+            wineWowPackages.stable
+          ];
         };
 
         devShells.default = pkgs.mkShell {
@@ -35,7 +93,7 @@
             rustPlatform.bindgenHook
 
             openssl
-            
+
             alejandra
             rust-analyzer
             (pkgs.fenix.stable.withComponents [
